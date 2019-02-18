667dd0feeb720c4cc29a5157ce6887e3dc77ef58
AnnZh <ann.zhurok@gmail.com>
2019-02-18 22:26:02 +0300

    Done

--- CartesianCoordinates.cs
+++ CartesianCoordinates.cs
@@ -7,8 +7,19 @@ namespace PadawansTask9
     {
         public static List<Point> GetNeighbors(Point point, int range, params Point[] points)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (points == null) throw new ArgumentNullException();
+            if (points.Length < 1) throw new ArgumentException();
+            if (range < 1) throw new ArgumentException();
+
+            List<Point> list = new List<Point>();
+            foreach (Point value in points)
+            {
+                if ((Math.Abs(point.X - value.X) <= range) && (Math.Abs(point.Y - value.Y) <= range))
+                {
+                    list.Add(value);
+                }
+            }
+            return list;
         }
     }
 }
